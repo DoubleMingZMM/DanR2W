195c1d686548fa0cd1992c209aeb09d0ac196591
Daniel <1602268076@qq.com>
2018-11-05 18:29:42 +0800

    update

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 // import Nav from '@/components/Nav/Nav'
+import * as d3 from 'd3'
 import getRouter from '@/router/router'
 import screenfull from 'screenfull'
-import { Layout, Menu, Icon, Breadcrumb, Badge } from 'antd'
+import { Layout, Menu, Icon, Breadcrumb, Badge, Drawer } from 'antd'
 import avater from '../../static/images/avater.jpg';
 const { Header, Sider, Content, Footer } = Layout
 const SubMenu = Menu.SubMenu
@@ -14,8 +15,39 @@ class App extends Component{
         super(props)
 
         this.state = {
-            collapsed: false
+            collapsed: false,
+            drawerVisible: false
         }
+    }
+
+    componentWillMount() {
+        const _this = this
+        let dragDeltaX, dragDeltaY
+        const items = d3.select(".drag-cursor")
+        let drag = d3.drag()
+            .on('start', function() {
+                const mousePosition = d3.mouse(this)
+                dragDeltaX = mousePosition[0]
+                dragDeltaY = mousePosition[1]
+                _this.getItemPosition(d3.event.sourceEvent.x - dragDeltaX, d3.event.sourceEvent.y - dragDeltaY)
+            })
+            .on('drag', function() {
+                _this.getItemPosition(d3.event.sourceEvent.x - dragDeltaX, d3.event.sourceEvent.y - dragDeltaY)
+            })
+            .on('end', function() {
+                const $container = document.getElementById('app')
+                const position = {
+                    x: d3.event.sourceEvent.x - dragDeltaX - $container.offsetLeft,
+                    y: d3.event.sourceEvent.y - dragDeltaY - $container.offsetTop
+                }
+            })
+        items.call(drag)
+    }
+
+    getItemPosition = (x, y) => {
+        debugger
+        const item = d3.select(".drag-cursor")
+        item.attr('style', `position:absolute;transform:translate(${x}px, ${y}px)`)
     }
 
     toggle = () => {
@@ -30,9 +62,36 @@ class App extends Component{
         }
     }
 
+    showDrawer = () => {
+        this.setState({
+            drawerVisible: true,
+        });
+    };
+
+    onClose = () => {
+        this.setState({
+            drawerVisible: false,
+        });
+    };
+
     render() {
         return(
-            <Layout style={{height: 'calc(100vh)'}}>
+            <Layout className="layout-style">
+                <div className="drag-cursor">
+                    <Icon type="setting" onClick={this.showDrawer}/>
+                </div>
+                <Drawer
+                    width="300px"
+                    title="基础设置"
+                    placement="right"
+                    closable={true}
+                    onClose={this.onClose}
+                    visible={this.state.drawerVisible}
+                >
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                    <p>Some contents...</p>
+                </Drawer>
                 <Sider
                     trigger={null}
                     collapsible
@@ -66,7 +125,7 @@ class App extends Component{
                         />
                         <Menu
                             mode="horizontal"
-                            style={{ lineHeight: '62px', float: 'right', borderBottom: 'none' }}
+                            style={{ lineHeight: '64px', float: 'right', borderBottom: 'none' }}
                         >
                             <Menu.Item key="full" className="border-bt-none-force">
                                 <i className="iconfont icon-fullscreen" onClick={this.fullScreen}/>
@@ -90,7 +149,7 @@ class App extends Component{
                             <Breadcrumb.Item>User</Breadcrumb.Item>
                             <Breadcrumb.Item>Bill</Breadcrumb.Item>
                         </Breadcrumb>
-                        <div style={{ padding: 24, background: '#fff', minHeight: 'calc(100vh - 200px)' }}>
+                        <div className="content-style">
                             {getRouter()}
                         </div>
                     </Content>
