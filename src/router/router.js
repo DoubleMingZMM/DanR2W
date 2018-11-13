8dd7dd897934b4d3b620f19cdd10e36bd5e960b3
Daniel <1602268076@qq.com>
2018-11-13 14:19:07 +0800

    update

--- src/router/router.js
+++ src/router/router.js
@@ -5,7 +5,7 @@ import Bundle from './Bundle'
 
 import Page1 from 'bundle-loader?lazy&name=page1!@/views/page1'
 import Page2 from 'bundle-loader?lazy&name=page2!@/views/page2'
-import Dashboard from 'bundle-loader?lazy&name=dashboard!@/views/dashboard'
+import Dashboard from 'bundle-loader?lazy&name=dashboard!@/views/Index'
 
 const createComponent = (component) => (props) => (
     <Bundle load={component}>
